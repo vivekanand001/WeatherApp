b7352f119b396eba0f17a55b0bf980d99386d2a7
Vivekanand Limbikai <127830407+vivekanand001@users.noreply.github.com>
2024-07-25 19:25:01 +0530

    Update geocoding.py

--- WeatherApp/geocoding.py
+++ WeatherApp/geocoding.py
@@ -1,6 +1,6 @@
 import requests
 def geocoding(cityname):
-    geour1=f"https://api.geoapify.com/v1/geocode/search?text={cityname}&apiKey=46897ab182d84f9bb348d41997b365c2"
+    geour1=f"https://api.geoapify.com/v1/geocode/search?text={cityname}&apiKey=add ur apikey"
     georesponse=requests.get(geour1).json()
     lat=georesponse["features"][0]["properties"]["lat"]
     lon=georesponse["features"][0]["properties"]["lon"]
